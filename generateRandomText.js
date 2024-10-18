dee5aa18e34d9f9dc95bc27daa851a6081468f38
Rajdev sah <120327885+rajdev-sah@users.noreply.github.com>
2024-10-18 12:44:57 +0545

    Delete generateRandomText.js

--- generateRandomText.js
+++
@@ -1,9 +0,0 @@
-import crypto from "crypto";
-
-const generateRandomText = () => {
-  const randomText = crypto.randomBytes(64).toString("hex");
-
-  console.log(randomText);
-};
-
-generateRandomText();
